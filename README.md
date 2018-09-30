60d0d5b6c19de78a704e2289a781e07e84da8a9f
hsuanhauliu <howardliu43@gmail.com>
2018-09-29 17:56:00 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Images Rename
+A simple script created for renaming files in a directory in a consistent manner. It renames files inside of the specified directory with a new name followed by a number, which increment by one starting from 1 for every file. A random number is generated each time the script is executed, and is then being appended to the end of the filename.
+
+## Use Cases
+Howard downloaded a ton of image data for training his machine learning model but too lazy to rename them one by one.
+
+## Getting started
+
+### Installing
+```
+git clone https://github.com/hsuanhauliu/images-rename.git
+```
+
+### Execution
+```
+python3 images-rename.py <directory> [-f filename] [-d]
+```
+
+- directory: the full path of the directory that contains the files you want to rename.
+- -f: optional parameter to set a name you want to use for renaming. By default, the name "file" would be used.
+- -d: disable default randomization in the naming.
+
+
+### Note
+- The script will only rename unhidden files (files with no "." at the beginning of their names). It will also ignore folders and files with no extension.
+- The randomization feature was added so that files won't be overwritten or deleted if one executes the program more than once in a row.
+
+## Screenshot
+![screenshot]( "screenshot")
---
+++ images_rename.py
@@ -0,0 +1,86 @@
+import argparse
+import random
+from os import listdir, rename, chdir
+from os.path import isfile, isdir, join
+
+def main():
+    args = parse_inputs()
+    dir_name = args.dir_path
+    randomization = True
+    filename = args.filename
+
+    if args.disable == True:
+        print("randomization disabled")
+        randomization = False
+    elif args.disable == False:
+        print("randomization enabled")
+
+    if not_a_directory(dir_name):
+        print("Error: not a directory")
+        raise ValueError
+
+    # change directory
+    chdir(dir_name)
+
+    # return a list of file names in the currect directory
+    myFiles = grab_files()
+
+    # start renaming files
+    rename_files(myFiles, filename, randomization)
+
+    return
+
+def rename_files(file_list, new_name, randomize):
+    """
+    file_list: list of filenames
+    name: name of the files that we will use for renaming
+    randomize: a flag indicating whether to add randomized number to prevent overwriting
+    """
+
+    counter = 0
+    randomized_name = ""
+
+    if randomize:
+        randomized_name = "_" + str(random.randint(0, 1000))
+
+    for filename in file_list:
+        counter += 1
+        extension = get_extension(filename)
+        new_filename = new_name + str(counter) + randomized_name + extension
+        rename(filename, new_filename)
+    print("Renamed", counter, "files")
+
+
+def get_extension(file_name):
+    """
+    Return the extension of the file_name.
+    """
+    return "." + file_name.split(".")[-1]
+
+def grab_files():
+    """
+    Return a list of unhidden files with extensions.
+    """
+    return [f for f in listdir(".") if isfile(f) and f[0] != "." and len(f.split(".")) > 1]
+
+def not_a_directory(dir_name):
+    """
+    Check if it is a directory.
+    """
+    return not isdir(dir_name)
+
+def parse_inputs():
+    """
+    Parse user inputs.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dir_path", type=str, help="enter directory path")
+    parser.add_argument("-d", "--disable", action='store_true',
+            help="disable adding randomized name in file names")
+    parser.add_argument("-f", "--filename", type=str, default="file",
+            help="input a file name that will be used for the files")
+
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    main()
